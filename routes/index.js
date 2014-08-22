8dd02e3c6d4a1c2af7ac278ee1e2233e75e2b1df
Nathan219 <Nathan219@gmail.com>
2014-08-21 19:21:50 -0700

    testing commit hooks

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-///fgsdfgdHFGDHDFGdfghdfghdfgh
+///fgsdfgdHFGDH
 module.exports = router;
