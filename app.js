2157963a5a5565e4a4e6213e9ed7a51001c7a7df
Nathan Meyers <nathan@runnable.com>
2015-09-23 23:13:47 -0700

    aw shit

--- app.js
+++ app.js
@@ -69,18 +69,6 @@ app.use('/users', users);
 //test
 //test
 //test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
